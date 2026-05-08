3967b7401d12888791750736b421169980333c03
Amr Elshamy <amr.120230247@ejust.edu.eg>
2026-05-08 19:28:38 +0300

    fix: add template and static folder paths in create_app()

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, 
+                template_folder="../templates",
+                static_folder="../static")
     app.secret_key = 'dev-secret-key'
 
     from src import menu, cart, order, tracking
