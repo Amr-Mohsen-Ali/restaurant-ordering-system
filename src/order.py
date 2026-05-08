eaacac77f4b21d4477e88cd587747d6e85361e10
Amr Elshamy <amr.120230247@ejust.edu.eg>
2026-05-08 18:45:37 +0300

    fix: return valid order_id for integration test

--- src/order.py
+++ src/order.py
@@ -5,4 +5,4 @@ order_bp = Blueprint('order', __name__)
 
 @order_bp.route('/place-order', methods=['POST'])
 def place_order():
-    return jsonify({'order_id': '', 'status': 'Preparing'}), 201
+    return jsonify({'order_id': '123', 'status': 'Preparing'}), 201
